f5fc528966495ab60800b82e3984c4ec7c8bf25d
I.G <ingrida.gudaityte@gmail.com>
2022-05-25 20:17:18 +0300

    naujo user'io sukurimas

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const MongoClient = require("mongodb").MongoClient;
 const mongoClient = new MongoClient("mongodb+srv://IngridaVIGI13:byuhblf77@cluster0.eipbj.mongodb.net/?retryWrites=true&w=majority");
 mongoClient.connect(function (err, client) {
-    const database = client.db("usersdb");
-    const collection = database.collection("users");
-    collection.countDocuments(function (err, result) {
+  const database = client.db("usersdb");
+  const collection = database.collection("users");
+    const newUser = { name: "Steve", age: 30 };
+    collection.insertOne(newUser, function (err, result) {
       if (err) {
-        return console.log(err);
+        return console.log("Failed to write an user");
       }
-      console.log(`Collection has ${result} documents`);
+      console.log(result);
       client.close();
     });
-  });
+//   collection.find().toArray(function (err, result) {
+//     if (err) {
+//       return console.log("errr", err);
+//     }
+//     console.log("Result:", result);
+//     client.close();
+//   });
+});
