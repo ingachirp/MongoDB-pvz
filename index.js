016e976f013fe764a1450a1230bfb0192b827725
I.G <ingrida.gudaityte@gmail.com>
2022-05-25 21:11:29 +0300

    duomenu rodymas per postman

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 const MongoClient = require("mongodb").MongoClient;
+const express = require("express");
+const app = express();
+const port = 3000;
 const mongoClient = new MongoClient("mongodb+srv://IngridaVIGI13:byuhblf77@cluster0.eipbj.mongodb.net/?retryWrites=true&w=majority");
+let database;
 mongoClient.connect(function (err, client) {
-    const database = client.db("usersdb");
-    const collection = database.collection("users");
-
-    collection.find({age: {$gt:40}}).toArray(function(err, result) {
-        console.log(result);
-        client.close();
-    })
-
-    // const newUsers = [
-    //   { name: "Tom", age: 30 },
-    //   { name: "Adam", age: 40 },
-    //   { name: "John", age: 60 },
-    // ];
-    // collection.insertMany(newUsers, function (err, result) {
-    //   if (err) {
-    //     return console.log("Failed to write an user");
-    //   }
-    //   collection.find().toArray(function (err, result) {
-    //     if (err) {
-    //       return console.log("errr", err);
-    //     }
-    //     console.log("Result:", result);
-    //     client.close();
-    //   });
-    // });
-  });
+  database = client.db("usersdb");
+});
+app.get("/", (req, res) => {
+  function sendResponse(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  }
+  database.collection("users").find().toArray(sendResponse);
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
