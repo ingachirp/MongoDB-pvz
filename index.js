3698d7f484ada374e402cadd6b1ab1cf8f8d094c
I.G <ingrida.gudaityte@gmail.com>
2022-05-25 20:30:00 +0300

    daug nauju vardu prijungimas i DB

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const MongoClient = require("mongodb").MongoClient;
 const mongoClient = new MongoClient("mongodb+srv://IngridaVIGI13:byuhblf77@cluster0.eipbj.mongodb.net/?retryWrites=true&w=majority");
 mongoClient.connect(function (err, client) {
-  const database = client.db("usersdb");
-  const collection = database.collection("users");
-    // const newUser = { name: "Steve", age: 30 };
-    // collection.insertOne(newUser, function (err, result) {
-    //   if (err) {
-    //     return console.log("Failed to write an user");
-    //   }
-    //   console.log(result);
-    //   client.close();
-    // });
-  collection.find().toArray(function (err, result) {
-    if (err) {
-      return console.log("errr", err);
-    }
-    console.log("Result:", result);
-    client.close();
+    const database = client.db("usersdb");
+    const collection = database.collection("users");
+    const newUsers = [
+      { name: "Tom", age: 30 },
+      { name: "Adam", age: 40 },
+      { name: "John", age: 60 },
+    ];
+    collection.insertMany(newUsers, function (err, result) {
+      if (err) {
+        return console.log("Failed to write an user");
+      }
+      collection.find().toArray(function (err, result) {
+        if (err) {
+          return console.log("errr", err);
+        }
+        console.log("Result:", result);
+        client.close();
+      });
+    });
   });
-});
